e1bd73b7cb0af85f0b671dbdf56cba947a1b5896
ErFaizanMemon <memonfaizan024@hotmail.com>
2023-10-02 19:36:52 -0400

    Env changes

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
-require("dotenv").config({ path: "./TEMPLATE"} );
-// import sequelize connection
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// const expressSanitizer = require("express-sanitizer");
-// const router = require("./Routes/router");
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
